b031fcd83161686967a0a8f3b5d9a8db4a6dbdd5
Sharanya  p shetty <159996195+Sharanyapshetty7@users.noreply.github.com>
2026-08-31 13:06:20 +0530

    Create pattern19.py

---
+++ pattern19.py
@@ -0,0 +1,19 @@
+n=int(input("enter: "))
+for i in range(n):
+    for j in range(n-i):
+        print("*",end="")
+    for j in range(2*i):
+        print(" ",end="")
+    for j in range(n-i):
+        print("*",end="")
+    print()
+
+for i in range(n,0,-1):
+    for j in range(n-i+1):
+        print("*",end="")
+    for j in range(2*i-2):
+        print(" ",end="")
+    for j in range(n-i+1):
+        print("*",end="")
+    print()
+    
