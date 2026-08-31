9afc3fcbad036905e1b5b4005530dba1c509524c
Sharanya  p shetty <159996195+Sharanyapshetty7@users.noreply.github.com>
2026-08-31 15:28:51 +0530

    Update pattern20.py

--- pattern20.py
+++ pattern20.py
@@ -15,3 +15,31 @@ for i in range(n-1,0,-1):
     for j in range(i,0,-1):
         print("*",end="")
     print()
+
+#or
+
+n = int(input("enter: "))
+
+spaces = 2*n - 2
+
+for i in range(1, 2*n):
+    stars = i
+
+    if i > n:
+        stars = 2*n - i
+
+    for j in range(stars):
+        print("*", end="")
+
+    for j in range(spaces):
+        print(" ", end="")
+
+    for j in range(stars):
+        print("*", end="")
+
+    print()
+
+    if i < n:
+        spaces -= 2
+    else:
+        spaces += 2
