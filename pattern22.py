5938f2c39ac0c9e761e672dd4a89f451d2480892
Sharanya  p shetty <159996195+Sharanyapshetty7@users.noreply.github.com>
2026-09-01 17:40:43 +0530

    Create pattern22.py

---
+++ pattern22.py
@@ -0,0 +1,10 @@
+n = int(input("enter: "))
+for i in range(2*n-1):
+    for j in range(2*n-1):
+        top=i
+        left=j
+        right=(2*n-2)-j
+        bottom=(2*n-2)-i
+        value = n-(min(top,bottom,left,right))
+        print(value, end=" ")
+    print()
