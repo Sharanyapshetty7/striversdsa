4c68e23b1f599e2a6e6cefa3f937f82b661b60fb
Sharanya  p shetty <159996195+Sharanyapshetty7@users.noreply.github.com>
2026-09-09 20:26:47 +0530

    Create pattern7.py

---
+++ pattern7.py
@@ -0,0 +1,12 @@
+n = 5
+
+for i in range(1, n + 1):
+    # spaces
+    for j in range(n - i):
+        print(" ", end="")
+
+    # stars
+    for j in range(2 * i - 1):
+        print("*", end="")
+
+    print()
